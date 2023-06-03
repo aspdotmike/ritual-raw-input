5a0c672968b634f066c093eabf496feceff9350e
Michael Vo <tkmdjvo@gmail.com>
2023-06-02 23:51:19 -0700

    first commit

--- app.py
+++ app.py
@@ -8,59 +8,20 @@ from langchain.chains import LLMChain, SequentialChain
 
 os.environ['OPENAI_API_KEY'] = apikey
 
-st.title('Problem')
-prompt_problem = st.text_input('What are you trying to solve?:')
+st.title('Actionable Input')
+prompt = st.text_input('Your input, such as a question?:')
 
-st.title('Challenge #1')
-prompt_challenge1 = st.text_input('What is one thing that makes solving this difficult:')
-
-st.title('Challenge #2')
-prompt_challenge2 = st.text_input('What is a second thing that makes solving this difficult:')
-
-st.title('Challenge #3')
-prompt_challenge3 = st.text_input('What is a third thing that makes solving this difficult:')
-
-#Prompt templates
-# title_template = PromptTemplate(
-#     input_variables = ['topic'],
-#     template = 'write me a youtube video title about {topic}'
-# )
-
-#Prompt templates
-# script_template = PromptTemplate(
-#     input_variables = ['title'],
-#     template = 'write me a youtube video script based on this title Title: {title}'
-# )
-
-#Prompt templates
-statement_template = PromptTemplate(
-    input_variables = ['problem', 'challenge1', 'challenge2', 'challenge3'],
-    template = 'Write me a problem statement based on this problem: {problem} and these challenges to solving the problem: {challenge1}, {challenge2}, {challenge3} using less than 240 characters.'
-)
-
-#Prompt templates
-title_template = PromptTemplate(
-    input_variables = ['statement'],
-    template = 'write me a project title about {statement} using less than 20 characters'
-)
-
-#Prompt templates
-competency_template = PromptTemplate(
-    input_variables = ['statement'],
-    template = 'provide a list of competenecies that would be helpful for an individual to have to solve: {statement}'
+question_template = PromptTemplate(
+    input_variables = ['raw_input'],
+    template='Make a question out of the following sentence: "{raw_input}"',
 )
 
 #Llms
 llm = OpenAI(temperature=0.9)
-statement_chain = LLMChain(llm=llm, prompt=statement_template, verbose=True, output_key='statement')
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title')
-competency_chain = LLMChain(llm=llm, prompt=competency_template, verbose=True, output_key='competencies')
-sequential_chain = SequentialChain(chains=[statement_chain, title_chain, competency_chain], input_variables=['problem', 'challenge1', 'challenge2', 'challenge3'], output_variables=['statement', 'title', 'competencies'], verbose = True)
+question_chain = LLMChain(llm=llm, prompt = question_template)
 
 
 # Show stuff to the screen if the user has entered a prompt
-if prompt_problem:
-    response = sequential_chain({'problem': prompt_problem, 'challenge1': prompt_challenge1, 'challenge2': prompt_challenge2, 'challenge3': prompt_challenge3})
-    st.write(response['statement'])
-    st.write(response['title'])
-    st.write(response['competencies'])
+if prompt:
+    response = question_chain.run(raw_input=prompt)
+    st.write(response)
